dfb9a0afb85770c085ddf212c10a9d11294254e3
Ivan <goleshovivan@yandex.by>
2019-03-12 23:42:56 +0300

    add to project maven

--- src/main/java/sample/Interfaces/AddressBook.java
+++ src/main/java/sample/Interfaces/AddressBook.java
@@ -1,4 +1,21 @@
 package sample.Interfaces;
 
+import sample.Object.Person;
+
+/**
+ * @author Bublik
+ * @version 1.0
+ *
+ */
+
 public interface AddressBook {
+/**
+ * @param Person
+ */
+
+    public void add(Person object);
+
+    public void update(Person object);
+
+    public void delete(Person object);
 }
--- src/main/java/sample/Interfaces/impls/CollectionsAddressBook.java
+++ src/main/java/sample/Interfaces/impls/CollectionsAddressBook.java
@@ -1,4 +1,29 @@
 package sample.Interfaces.impls;
 
-public class CollectionsAddressBook {
+import sample.Interfaces.AddressBook;
+import sample.Object.Person;
+
+import java.util.ArrayList;
+
+public class CollectionsAddressBook implements AddressBook {
+
+
+    private ArrayList<Person> personArrayList=new ArrayList<Person>();
+
+
+    public void add(Person object) {
+        personArrayList.add(object);
+    }
+
+    public void update(Person object) {
+
+    }
+
+    public void delete(Person object) {
+        personArrayList.remove(object);
+    }
+
+    public ArrayList<Person> getPersonList(){
+        return personArrayList;
+    }
 }
